44ca30839896ef6623fed264be962d1ad143c755
Andrea Telatin <andrea@telatin.com>
2020-05-29 09:08:48 +0100

    1.1.2 pre-release

--- base.cpp
+++ base.cpp
@@ -18,7 +18,7 @@ const char ref_char = '>';  // reference prefix for "counts" output
 
 const string VERSION = "%prog 1.1.2"
 	"\nCopyright (C) 2014-2019 Giovanni Birolo and Andrea Telatin\n"
-	"License MIT"
+	"https://github.com/telatin/covtobed - License MIT"
 	".\n"
 	"This is free software: you are free to change and redistribute it.\n"
 	"There is NO WARRANTY, to the extent permitted by law.";
